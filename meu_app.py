11585ea828d7d0f1d21e6614fa5f94e2ab736a1b
unknown <Eduardo Filip@ITJ0019.itajai.quay.com.br>
2021-02-09 17:50:17 -0300

    Operações com Python

--- meu_app.py
+++ meu_app.py
@@ -1,6 +1,11 @@
-print ("Hello World!")
-a = 2
-b = 3
-soma = a + b
+a = int(input("Digite um número: "))
+b = int(input("Digite outro número: "))
 
-print (soma)
+operacao = str(input("Qual operação você deseja realizar? \n Escolha uma das opções abaixo: \n M - Multiplicação \n D - Divisão \n S - Soma \n U - Subtracao \n "))
+
+if operacao == 'M':
+    operacao = a * b
+    print("O resultado da operação é: {}" .format(operacao))
+elif operacao == 'D':
+    operacao = a / b
+    print("O resultado da operação é: {}" .format(operacao))
