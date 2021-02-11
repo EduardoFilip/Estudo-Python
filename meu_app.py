3f89d40cf664356488b603c9356ae3b8bf4dfea9
unknown <Eduardo Filip@ITJ0019.itajai.quay.com.br>
2021-02-11 09:16:54 -0300

    Operações em Python

--- meu_app.py
+++ meu_app.py
@@ -1,11 +1,19 @@
 a = int(input("Digite um número: "))
 b = int(input("Digite outro número: "))
+mult = a * b
+div = a / b
+som = a + b
+sub = a - b
 
-operacao = str(input("Qual operação você deseja realizar? \n Escolha uma das opções abaixo: \n M - Multiplicação \n D - Divisão \n S - Soma \n U - Subtracao \n "))
+operacao = input("Qual operação você deseja realizar? \n Escolha uma das opções abaixo: \n M - Multiplicação \n D - Divisão \n S - Soma \n U - Subtracao \n ")
 
-if operacao == 'M':
-    operacao = a * b
-    print("O resultado da operação é: {}" .format(operacao))
-elif operacao == 'D':
-    operacao = a / b
-    print("O resultado da operação é: {}" .format(operacao))
+if operacao == 'M' or operacao == 'm':
+    print("O resultado da operação é: {} " .format(mult))
+elif operacao == 'D' or operacao == 'd':
+    print("O resultado da operação é: {} " .format(div))
+elif operacao == 'S' or operacao == 's':
+    print("O resultado da operação é: {} " .format(som))
+elif operacao == 'U' or operacao == 'u':
+    print("O resultado da operação é: {} " .format(sub))
+else:
+    print("Operação escolhida inválida!")
